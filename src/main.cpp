ed5b2e1f854c5f85a8baaf4afa8d45ad7bf450ae
github-username <ffan005@ucr.edu>
2021-02-15 12:26:21 -0800

    modified main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -5,9 +5,17 @@ using namespace std;
 
 int main()
 {
-    Rectangle rect;
-    rect(2,3);
-    cout << "Rectangle area: " << rect.area() << endl;
-    cout << "Rectangle perimeter: " << rect.perimeter() << endl;
+    Rectangle rect1;
+    rect1(2,3);
+    cout << "Rectangle 1, width 2 height 3" << endl;
+    cout << "Rectangle area: " << rect1.area() << endl;
+    cout << "Rectangle perimeter: " << rect1.perimeter() << endl;
+
+    Rectangle rect2;
+    rect2.set_width(4);
+    rect2.set_height(5);
+    cout << "Rectangle 2, width 4 height 5" << endl;
+    cout << "Rectangle area: " << rect2.area() << endl;
+    cout << "Rectangle perimeter: " << rect2.perimeter() << endl;
     return 0;
 }
