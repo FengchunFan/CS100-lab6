ccae1f018dae926d54799939a13b960c61a8195d
github-username <ffan005@ucr.edu>
2021-02-15 12:45:47 -0800

    added more test

--- tests/test.cpp
+++ tests/test.cpp
@@ -7,4 +7,34 @@ int main(int argc, char **argv) {
   return RUN_ALL_TESTS();
 }
 
+TEST(RectangleTest, Constructor){
+	Rectangle* rect = new Rectangle();
+	Expect_EQ(rect->area(),0);	
+}
+
+TEST(RectangleTest, Constructor){
+        Rectangle* rect = new Rectangle();
+        Expect_EQ(rect->perimeter(),0);
+}
+
+TEST(RectangleTest, Constructor_Normal){
+        Rectangle* rect = new Rectangle(2,3);
+        Expect_EQ(rect->area(),6);
+}
+
+TEST(RectangleTest, Constructor_Normal){
+        Rectangle* rect = new Rectangle(2,3);
+        Expect_EQ(rect->perimeter(),10);
+}
+
+TEST(RectangleTest, Constructor_WZero){
+        Rectangle* rect = new Rectangle(0,3);
+        Expect_EQ(rect->area(),0);
+}
+
+TEST(RectangleTest, Constructor_HZero){
+        Rectangle* rect = new Rectangle(3,0);
+        Expect_EQ(rect->area(),0);
+}
+
 
